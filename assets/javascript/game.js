b40ac1ba5d4068326ae9de06f69cb2062399239b
Will <Will.irvin18@gmail.com>
2019-08-02 19:55:58 -0400

    done!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,25 +13,27 @@ var what = document.getElementById("yourGuess");
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var soFar = [];
 
+
 document.onkeyup = function (event) {
   
 
   var userGuess = event.key;
-
+  what.textContent = "Your Guesses so far: " + soFar.push(userGuess);
+  
 
   var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
   if (userGuess === computerGuess) {
 
     winss.textContent = "Wins: " + wins++;
-    what.textContent = "Your Guesses so far: " + soFar.push(userGuess);
+    what.textContent = "Your Guesses so Far: " + soFar;
     guess.textContent = "Guesses Left: " + guessesLeft--;
     
 
 
   }
   else {
-    what.textContent = "Your Guesses so far: " + soFar.push(userGuess);
+    what.textContent = "Your Guesses so Far: " + soFar;
     guess.textContent = "Guesses Left: " + guessesLeft--;
     
   }
@@ -39,6 +41,12 @@ document.onkeyup = function (event) {
   if (guessesLeft === 0) {
     loser.textContent = "Losses: " + losses++;
         guessesLeft= 9;
+        
+  } 
+  if (soFar.length === 9) {
+    soFar.length = 0;
+    
+
   }
 
 
