da4d20a71a7d7906118d5ccf7a4642be224a501e
Will <Will.irvin18@gmail.com>
2019-08-02 18:18:02 -0400

    almost there!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,47 @@
-var wins = 0;
-var losses = 0;
+var wins = 1;
+var losses = 1;
 var guessesLeft = 9;
 
-var winss = document.getElementById("wins")
-var loser = document.getElementById("losses")
-var guess = document.getElementById("guessesLeft")
-var what = document.getElementById("yourGuess")
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-document.onkeyup = function(event) {
-    
-    var userGuess = event.key;
-    
-    
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  if (userGuess === computerGuess){
 
-    winss.textContent = winss++;
+
+
+
+var winss = document.getElementById("wins");
+var loser = document.getElementById("losses");
+var guess = document.getElementById("guessesLeft");
+var what = document.getElementById("yourGuess");
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+document.onkeyup = function (event) {
+  
+
+  var userGuess = event.key;
+
+
+  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+  if (userGuess === computerGuess) {
+
+    winss.textContent = wins++;
     what.textContent = userGuess;
     guess.textContent = guessesLeft--;
+    
+
 
   }
   else {
-      loser.textContent = loser++;
-      what.textContent = userGuess;
-      guess.textContent = guessesLeft--;
+    
+    what.textContent = userGuess;
+    guess.textContent = guessesLeft--;
+    what.textContent = userGuess;
   }
-  
+
+  if (guessesLeft === 0) {
+    loser.textContent = losses++;
+        guessesLeft= 9;
+  }
+
+
 
 };
